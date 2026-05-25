f2f388ef8fd747891391530565ab54082cba965b
Artur Piterov <me@piterov.xyz>
2026-05-25 15:02:31 +0300

    temporary debug

--- cmd/juun/main.go
+++ cmd/juun/main.go
@@ -7,11 +7,13 @@ import (
 )
 
 func main() {
-	var sysInfo indexer.InfoCache
+	var sysInfo *indexer.InfoCache
 
 	fmt.Println("juun client starting...")
 	sysInfo, err := indexer.RunIndexer()
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
+
+	fmt.Println(sysInfo)
 }
